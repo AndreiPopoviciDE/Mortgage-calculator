5fda215ca09e27a973bc40a82ab9996a1984e45c
Bogdan Nedelcu <bogdanned32@gmail.com>
2024-05-13 10:37:10 +0200

    feat: feedback

---
+++ Feedback.md
@@ -0,0 +1,6 @@
+## Feedback
+
+- use TypeScript
+- migrate to styled-components
+- write unit test for helpers.js
+- the button component can be an antipattern - we can discuss this in a technical call
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,12 +1,27 @@
 export function calculateMonthlyPayment(loanAmount, interestRate, lengthLoan) {
+  // FEEDBACK: do not modify function arguments
+  // imutabiliyt - pure functions - easier to test - side effect
   interestRate = interestRate / 100 / 12;
-  lengthLoan = lengthLoan * 12;
+  const lengthLoanParsed = lengthLoan * 12;
 
-  var monthlyPayment =
+  // red flag
+  let monthlyPayment =
     (loanAmount * interestRate * Math.pow(1 + interestRate, lengthLoan)) /
     (Math.pow(1 + interestRate, lengthLoan) - 1);
+    
   monthlyPayment = +monthlyPayment.toFixed(0);
+
   return monthlyPayment.toLocaleString('en-US');
+}
+
+// SOLID - Single responsibility principle
+
+// format function
+export function formatCurrency(value) {
+  return value.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
 }
 
 
